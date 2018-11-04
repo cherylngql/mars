1693c27e4638e9eda43be396897674a376417e69
Cheryl Ng <cherylngql@gmail.com>
2018-11-04 01:01:40 -0500

    display info on click

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 const xhr = new XMLHttpRequest();
 
-function getData() {
-  const boturl = '//headlight-tournament-1.herokuapp.com/bots';
-  const nodeurl = '//headlight-tournament-1.herokuapp.com/nodes';
-  xhr.open("GET", boturl);
+function getBotData() {
+  const url = '//headlight-tournament-3.herokuapp.com/bots';
+  xhr.open("GET", url);
   xhr.responseType = "json";
   xhr.send();
   xhr.onreadystatechange = () => {
@@ -11,39 +10,24 @@ function getData() {
       bots = xhr.response.Bots
     }
   }
-  // xhr.open("GET", nodeurl);
-  // xhr.responseType = "json";
-  // xhr.send();
-  // xhr.onreadystatechange = () => {
-  //   if (xhr.readyState === 4 && xhr.status === 200) {
-  //     nodes = xhr.response.Nodes
-  //   }
-  // }
+}
 
+function getNodeData() {
+  const url = '//headlight-tournament-3.herokuapp.com/nodes';
+  xhr.open("GET", url);
+  xhr.responseType = "json";
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      nodes = xhr.response.Nodes
+    }
+  }
 }
 
 let bots;
 let nodes;
-// Sample request
 
-// let getBots = function(response) {
-  // const body = document.getElementsByTagName('BODY')[0];
-  // bots = response.Bots;
-  // for (let i = 0; i < bots.length; i++) {
-  //   let botDiv = document.createElement('div');
-  //   let bot = bots[i]
-  //   let markup = `
-  //     <h3>id: ${bot.Id}</h3>
-  //     <p>location: (${bot.Location.X},${bot.Location.Y})</p>
-  //     <p>score: ${bot.Score}</p>
-  //   `
-  //   botDiv.innerHTML = markup
-  //   body.appendChild(botDiv);
-  // }
-// }
-
-// let getNodes = function(response) {
-//   nodes = response.Nodes;
-// }
-setInterval(getData,1000);
-// sendGETRequest("/nodes", getNodes);
+setInterval(async () => {
+  getNodeData();
+  setTimeout(getBotData, 100);
+},1000);
--- public/view.js
+++ public/view.js
@@ -18,19 +18,94 @@ for (let h = 0; h < height; h++) {
 
 document.getElementById("board").append(table)
 
-function update(bots) {
-  const prevBots = Array.from(document.getElementsByClassName('bot'));
-  console.log(prevBots)
+function displayBot(bot) {
+  const selectedBotLeft = document.querySelector('#selected-bot .left');
+  const selectedBotRight = document.querySelector('#selected-bot .right');
+  const keys = 
+  `
+  <p>Bot ID :</p>
+  <p>Location :</p>
+  <p>Score :</p>
+  <p>Claimes :</p>
+  `;
+  selectedBotLeft.innerHTML = keys;
+  const values = 
+  `
+  <p>${bot.Id}</p>
+  <p>(${bot.Location.X},${bot.Location.Y})</p>
+  <p>${bot.Score}</p>
+  <p>${bot.Claims ? 'None' : bot.Claims.join(', ')}</p>
+  `;
+  selectedBotRight.innerHTML = values;
+}
+
+function displayNode(node) {
+  const selectedNodeLeft = document.querySelector('#selected-node .left');
+  const selectedNodeRight = document.querySelector('#selected-node .right');
+  const keys = 
+  `
+  <p>Node ID :</p>
+  <p>Location :</p>
+  <p>Value :</p>
+  <p>Claimer :</p>
+  `;
+  selectedNodeLeft.innerHTML = keys;
+  const values = 
+  `
+  <p>${node.Id}</p>
+  <p>(${node.Location.X},${node.Location.Y})</p>
+  <p>${node.Value}</p>
+  <p>${node.ClaimedBy}</p>
+  `;
+  selectedNodeRight.innerHTML = values;
+}
+
+function onClick(event) {
+  document.getElementById('click').style.display = 'none';
+  if (event.target.bot && event.target.node) {
+    const bot = event.target.bot;
+    const node = event.target.node;
+    displayBot(bot)
+    displayNode(node)
+  } else if (event.target.bot) {
+    const bot = event.target.bot;
+    displayBot(bot)
+  } else {
+    const node = event.target.node;
+    displayNode(node)
+  }
+}
+
+function update() {
+  const boardDiv = document.getElementById('board');
+  const prevBots = Array.from(boardDiv.getElementsByClassName('bot'));
+  const prevNodes = Array.from(boardDiv.getElementsByClassName('node'));
+
   for (let i = 0; i < prevBots.length; i++) {
     prevBots[i].classList.remove('bot')
+    prevBots[i].bot = undefined
+    prevBots[i].removeEventListener('click', onClick)
+  }
+  for (let i = 0; i < prevNodes.length; i++) {
+    prevNodes[i].classList.remove('node')
+    prevNodes[i].removeEventListener('click', onClick)
   }
 
   for (let i = 0; i < bots.length; i++) {
     const x = bots[i].Location.X;
     const y = bots[i].Location.Y;
-    board[x][y].classList.add('bot')
+    board[x][y].classList.add('bot');
+    board[x][y].bot = bots[i];
+    board[x][y].addEventListener('click', onClick)
+  }
+
+  for (let i = 0; i < nodes.length; i++) {
+    const x = nodes[i].Location.X;
+    const y = nodes[i].Location.Y;
+    board[x][y].classList.add('node')
+    board[x][y].node = nodes[i]
+    board[x][y].addEventListener('click', onClick)
   }
 }
 
-
-setInterval(() => {update(bots)}, 2000)
+setInterval(update, 2000)
