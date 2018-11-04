70a8a867954fcde199a6bf885c4071c907b4523a
Cheryl Ng <cherylngql@gmail.com>
2018-11-03 20:54:02 -0400

    server

---
+++ main.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log(`App listening on port ${port}...`)
+});
